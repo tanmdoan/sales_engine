9ee5375b673d218aab40a152ed06c41e80de6419
Tan Doan <tdoan87@gmail.com>
2014-06-30 11:45:49 -0600

    initial commit

---
+++ lib/sales_engine.rb
@@ -0,0 +1,15 @@
+require './lib/merchant'
+require './lib/merchant_repository'
+require './lib/items'
+require './lib/items_repository'
+require './lib/invoice'
+require './lib/invoice_repository'
+require './lib/transaction'
+require './lib/transaction_repository'
+require './lib/customer'
+require './lib/customer_repository'
+require 'csv'
+
+class SalesEngine
+
+end
