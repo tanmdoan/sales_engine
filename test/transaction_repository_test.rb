0a46cc0661d5e323d4b660752ec731be51e80a54
timproctor <timandcamiproctor@gmail.com>
2014-07-01 20:00:53 -0600

    finished searching for transactions test/lib

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -15,4 +15,64 @@ class TransactionRepositoryTest < Minitest::Test
     refute transaction_repository.transactions.empty?
   end
 
+  def test_it_can_find_by_transaction_id
+    results = transaction_repository.find_by_id(3)
+    assert_equal 1, results.count
+    results.each do |transaction|
+      assert_equal "3", transaction.id
+    end
+  end
+
+  def test_it_can_find_by_invoice_id
+    results = transaction_repository.find_by_invoice_id(1)
+    assert_equal 1, results.count
+    results.each do |transaction|
+      assert_equal "1", transaction.invoice_id
+    end
+  end
+
+  def test_it_can_find_by_credit_card_number
+    results = transaction_repository.find_by_credit_card_number(4654405418249632)
+    assert_equal 1, results.count
+    results.each do |transaction|
+      assert_equal "4654405418249632", transaction.credit_card_number
+    end
+  end
+
+  def test_it_can_find_all_by_credit_card_number
+    results = transaction_repository.find_all_by_credit_card_number(4654405418249632)
+    assert_equal 1, results.count
+    results.each do |transaction|
+      assert_equal "4654405418249632", transaction.credit_card_number
+    end
+  end
+
+  def test_it_can_find_by_result
+    results = transaction_repository.find_by_result("success")
+    assert_equal 1, results.count
+    results.each do |transaction|
+      assert_equal "success", transaction.result
+    end
+  end
+
+  def test_it_can_find_all_by_result
+    results = transaction_repository.find_all_by_result("success")
+    assert_equal 9, results.count
+    results.each do |transaction|
+      assert_equal "success", transaction.result
+    end
+  end
+
+  def test_it_can_find_all_instances_of_transactions
+    results = transaction_repository.transactions.count
+    assert 10 <= results
+  end
+
+  def test_it_can_pull_a_random_transaction_instance
+    results = transaction_repository.random
+    assert_equal 1, results.count
+  end
+
+
+
 end
