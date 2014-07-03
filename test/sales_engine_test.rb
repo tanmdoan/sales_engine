2167f0dfc58c4bd7c2385f2bf5636ca6b82aaf4a
timproctor <timandcamiproctor@gmail.com>
2014-07-03 12:18:52 -0600

    added sales engine test for merchants getting all items

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -10,9 +10,14 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_customer_repository_exist_on_engine_start_up
-    # engine = SalesEngine.new
-    # engine.startup
     assert_equal 1000, engine.customer_repository.customers.count
     assert_equal "Joey", engine.customer_repository.customers.first.first_name
+  end
+
+  def test_sales_engine_can_find_all_items_sold_by_a_merchant
+    merchant    = engine.merchant_repository.find_by_name("Schroeder-Jerde")
+    merchant_id = merchant.id
+    items       = item_repository.find_by_merchant_id(merchant_id)
+    assert_equal 9, merhcant.items.count
   end
 end
