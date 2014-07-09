68882062f2bdc71245fd5be9d350bacaaf569f34
timproctor <timandcamiproctor@gmail.com>
2014-07-09 12:57:53 -0600

    customer_repo tests all pass

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -2,59 +2,66 @@ require './test/test_helper'
 
 
 class CustomerRepositoryTest < Minitest::Test
- attr_reader :customer_repository
+  attr_reader :customer_repository
 
- def setup
-   @customer_repository ||= CustomerRepository.load(self, "./data/fixtures/customers_sample.csv")
- end
+  def setup
+    @customer_repository ||= CustomerRepository.load(self, "./data/fixtures/customers_sample.csv")
+  end
 
- def test_that_customer_repository_contains_customer_data
-   assert 10 <= customer_repository.customers.count
- end
+  def test_that_customer_repository_contains_customer_data
+    assert 10 <= customer_repository.customers.count
+  end
 
- def test_to_verify_that_customer_repository_is_not_empty
-   refute customer_repository.customers.empty?
- end
+  def test_to_verify_that_customer_repository_is_not_empty
+    refute customer_repository.customers.empty?
+  end
 
- def test_it_can_find_by_customers_id
-   results = customer_repository.find_by_id(1)
-     assert_equal 1, results.id
- end
+  def test_it_can_find_by_customers_id
+    results = customer_repository.find_by_id(1)
+    assert_equal 1, results.id
+  end
 
- def test_it_can_find_by_customers_first_name
-   results = customer_repository.find_by_first_name("Joey")
-      assert_equal "Joey", results.first_name
- end
+  def test_it_can_find_by_customers_first_name
+    results = customer_repository.find_by_first_name("Joey")
+    assert_equal "Joey", results.first_name
+  end
 
- def test_it_can_find_by_customers_last_name
-   results = customer_repository.find_by_last_name("Ondricka")
-     assert_equal "Ondricka", results.last_name
- end
+  def test_it_can_find_by_customers_last_name
+    results = customer_repository.find_by_last_name("Ondricka")
+    assert_equal "Ondricka", results.last_name
+  end
 
- def test_it_can_find_all_customers_by_first_name
-   results = customer_repository.find_all_by_first_name("Joey")
-   assert_equal 2, results.count
-   results.each do |customer|
-     assert_equal "Joey", customer.first_name
-   end
- end
+  def test_it_can_find_all_customers_by_first_name
+    results = customer_repository.find_all_by_first_name("Joey")
+    assert_equal 2, results.count
+    results.each do |customer|
+      assert_equal "Joey", customer.first_name
+    end
+  end
 
- def test_it_can_find_all_customers_by_last_name
-   results = customer_repository.find_all_by_last_name("Ondricka")
-   assert_equal 2, results.count
-   results.each do |customer|
-     assert_equal "Ondricka", customer.last_name
-   end
- end
+  def test_it_can_find_all_customers_by_last_name
+    results = customer_repository.find_all_by_last_name("Ondricka")
+    assert_equal 2, results.count
+    results.each do |customer|
+      assert_equal "Ondricka", customer.last_name
+    end
+  end
 
- def test_it_can_find_all_instances_of_customer
-   customer_repo = CustomerRepository.load(self, './data/customers.csv')
-   assert 100 <= customer_repo.customers.count
- end
+  def test_it_can_find_all_instances_of_customer
+    customer_repo = CustomerRepository.load(self, './data/customers.csv')
+    assert 100 <= customer_repo.customers.count
+  end
 
- def test_it_can_pull_a_random_customers_instance
-   results = customer_repository.random
-   refute results.empty?, results
- end
+  def test_it_can_pull_a_random_customers_instance
+    customer_one = customer_repository.random
+    customer_two = customer_repository.random
 
+    10.times do
+      break if customer_one.id != customer_two.id
+      customer_two = customer_repository.random
+    end
+
+    refute customer_one.id. == customer_two.id
+
+  end
 end
